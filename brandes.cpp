04498b83b816fca8cfc4a1555d0d3b3c931a755d
Mikołaj Walczak <mikiwalczak@gmail.com>
2016-12-22 10:53:21 +0100

    Dodanie wczytywania wejscia

--- brandes.cpp
+++ brandes.cpp
@@ -1,14 +1,10 @@
 #include "brandes.h"
-
-namespace {
-    class BrandesWorker {
-
-    };
-}
+#include <iostream>
 
 Brandes::Brandes(const size_t &threads_count, const std::string &input_filename, const std::string &output_filename)
         : pool(threads_count) {
     input_file.open(input_filename);
+    load_input();
     output_file.open(output_filename);
 }
 
@@ -18,5 +14,20 @@ Brandes::~Brandes() {
 }
 
 void Brandes::calculate() {
+    save_output();
+}
+
+void Brandes::load_input() {
+    Node from, to;
+    while (input_file >> from >> to) {
+        auto from_iterator = graph.find(from);
+        if (from_iterator == graph.end()) {
+            from_iterator = graph.insert(std::make_pair(from, BrandesNode{ from })).first;
+        }
+        from_iterator->second.edges.push_back(to);
+    }
+}
+
+void Brandes::save_output() {
 
 }
--- brandes.h
+++ brandes.h
@@ -4,14 +4,33 @@
 #include <cstddef>
 #include <string>
 #include <fstream>
+#include <map>
+#include <vector>
 
 #include "thread_pool.h"
 
 class Brandes {
+
+    using Node = int;
+    using Weight = double;
+
+    struct BrandesNode {
+        Node id;
+        std::vector<Node> edges;
+        Weight weight;
+
+        BrandesNode(Node id)
+                : id{id}, edges{}, weight{0} {}
+    };
+
 private:
     std::ifstream input_file;
     std::ofstream output_file;
     ThreadPool pool;
+    std::map<Node, BrandesNode> graph;
+
+    void load_input();
+    void save_output();
 
 public:
     Brandes(const size_t &threads_count, const std::string &input_filename, const std::string &output_filename);
