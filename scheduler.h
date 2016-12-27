dec18766f273e7fd8ea257201151c58e84eccec6
Mikołaj Walczak <mikiwalczak@gmail.com>
2016-12-27 13:29:37 +0100

    Scheduler comments

--- scheduler.h
+++ scheduler.h
@@ -9,30 +9,80 @@
 #include <thread>
 #include <queue>
 
+/**
+ * Creates Scopes instances and performs execute function for every scheduled
+ * Task.
+ * @tparam Scope class to
+ * @tparam ScopeArgs argument of the constructor - has to be given explicitly
+ * (up until to c++17 it cannot be deduced from context)
+ * @tparam Task type to the schedule (argument of Scope::execute function) -
+ * has to be given explicitly
+ */
 template <typename Scope, typename ScopeArgs, typename Task>
 class Scheduler {
 private:
+    /**
+     * Argument to be passed Scope instances constructor
+     */
     ScopeArgs scope_args;
 
+    /**
+     * Marks if scheduler is about to finish.
+     */
     std::atomic_bool terminating;
+    /**
+     * Mutex to guarantee thread safety
+     */
     std::mutex mutex;
+    /**
+     * Condition for workers to wait for tasks
+     */
     std::condition_variable workers_condition;
+    /**
+     * Condition for threads to wait for all workers to finish
+     */
     std::condition_variable wait_condition;
 
+    /**
+     * Vector of worker threads
+     */
     std::vector<std::thread> threads;
+    /**
+     * Queue of scheduled tasks
+     */
     std::queue<Task> tasks;
 
+    /**
+     * Function performed by each thread.
+     * @param scheduler - pointer to `this`
+     */
     static void worker(Scheduler<Scope, ScopeArgs, Task>* scheduler);
 public:
+    /**
+     * Creates new scheduler.
+     * @param threads_count - number of threads to be used by scheduler.
+     * @param args - argument to be passed to Scope instances
+     */
     Scheduler(std::size_t threads_count, ScopeArgs args);
     ~Scheduler();
 
+    // Non copyable
     Scheduler(const Scheduler &) = delete;
     Scheduler &operator=(const Scheduler &) = delete;
+    // But movable
     Scheduler(Scheduler &&) = default;
     Scheduler &operator=(Scheduler &&) = default;
 
+    /**
+     * Schedule new task.
+     * Scheduled task will be performed in FIFO order.
+     * @param task - task to be scheduled.
+     */
     void schedule(Task task);
+    /**
+     * Stops execution of thread that performed join until all scheduled tasks
+     * are finished.
+     */
     void join();
 };
 
